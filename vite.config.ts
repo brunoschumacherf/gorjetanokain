65daacedae6424a82db191f3c95611c4fb38de20
Bruno Schumacher F. Souza <brunoschumacherfsouza@gmail.com>
2026-02-24 16:32:22 -0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    historyApiFallback: true,
-  },
-  preview: {
-    port: 5173,
-  },
 })
